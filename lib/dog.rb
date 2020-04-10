a7d38a7ce78e5f1c78ede044d12f37c2e3a27316
Willis Aybar <willisaybar@gmail.com>
2020-04-10 00:07:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,6 +9,15 @@ attr_reader :id
     @breed = breed
   end
 
-
+  def self.create_table
+      sql =  <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          breed TEXT
+          )
+          SQL
+      DB[:conn].execute(sql)
+    end
 
 end
