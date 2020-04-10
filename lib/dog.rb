3702474a62d4695e5e30beea646197a7b0e7e916
Willis Aybar <willisaybar@gmail.com>
2020-04-10 00:18:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
 attr_accessor :name, :breed
 attr_reader :id
 
-  def initialize(id=nil, name:, breed:)
+  def initialize(id: nil, name:, breed:)
       @id = id
       @name = name
       @breed = breed
@@ -87,5 +87,5 @@ attr_reader :id
         sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
         DB[:conn].execute(sql, self.name, self.breed, self.id)
       end
-      
+
 end
